56170770ea934d019750c11af7babce9fee1ca6d
FrostLGC <alvaherbart.353@gmail.com>
2025-05-10 20:43:40 +0700

    feat: penambahan fitur update task dan delete task

--- frontend/Project-Management/src/pages/Admin/CreateTask.jsx
+++ frontend/Project-Management/src/pages/Admin/CreateTask.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DashboardLayout from "../../components/Layouts/DashboardLayout";
 import { PRIORITY_DATA } from "../../utils/data";
 import axiosInstance from "../../utils/axiosInstance";
@@ -11,6 +11,8 @@ import SelectDropDown from "../../components/Inputs/SelectDropDown";
 import SelectUsers from "../../components/Inputs/SelectUsers";
 import TodoListInput from "../../components/Inputs/TodoListInput";
 import AddAttachmentsInput from "../../components/Inputs/AddAttachmentsInput";
+import Modal from "../../components/modal";
+import DeleteAlert from "../../components/DeleteAlert";
 
 const CreateTask = () => {
   const location = useLocation();
@@ -81,7 +83,36 @@ const CreateTask = () => {
   };
 
   // update Task
-  const updateTask = async () => {};
+  const updateTask = async () => {
+    setLoading(true);
+
+    try {
+      const todoList = taskData.todoChecklist?.map((item) => {
+        const prevTodoCheckList = currentTask?.todoChecklist || [];
+        const matchedTask = prevTodoCheckList.find((task) => task.text == item);
+
+        return {
+          text: item,
+          completed: matchedTask ? matchedTask.completed : false,
+        };
+      });
+
+      const response = await axiosInstance.put (
+        API_PATH.TASK.UPDATE_TASK(taskId),
+        {
+          ...taskData,
+          dueDate: new Date(taskData.dueDate).toISOString(),
+          todoChecklist: todoList,
+        }
+      );
+
+      toast.success("Task Updated Succesfully");
+    } catch (error) {
+      console.error("Error creating task:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const handleSubmit = async () => {
     setError(null);
@@ -120,10 +151,58 @@ const CreateTask = () => {
   };
 
   // mendapatkan info task dari id
-  const getTaskDetailsByID = async () => {};
+  const getTaskDetailsByID = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATH.TASK.GET_TASK_BY_ID(taskId)
+      );
+
+      if (response.data) {
+        const taskInfo = response.data;
+        setCurrentTask(taskInfo);
+
+        setTaskData((prevState) => ({
+          title: taskInfo.title,
+          description: taskInfo.description,
+          priority: taskInfo.priority,
+          dueDate: taskInfo.dueDate
+            ? moment(taskInfo.dueDate).format("YYYY-MM-DD")
+            : null,
+          assignedTo: taskInfo?.assignedTo?.map((item) => item?._Id) || [],
+          todoChecklist: 
+            taskInfo?.todoChecklist?.map((item) => item?.text) || [], 
+          attachments: taskInfo?.attachments || [],
+        }));
+      }
+    } catch (error) {
+      console.error("Error fetching users", error);
+    }
+  };
 
   // menghapus task
-  const deleteTask = async () => {};
+  const deleteTask = async () => {
+    try {
+      await axiosInstance.delete(API_PATH.TASK.DELETE_TASK(taskId));
+
+      setOpenDeleteAlert(false);
+      toast.success("Expense details deleted succesfully");
+      navigate('/admin/tasks')
+    } catch (error) {
+      console.error(
+        "Error deleting expense details",
+        error.respone?.data?.message || error.message
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (taskId) {
+      getTaskDetailsByID(taskId);
+    }
+
+    return () => {};
+  }, [taskId]);
+  
 
   return (
     <DashboardLayout activeMenu="Create Task">
@@ -260,6 +339,18 @@ const CreateTask = () => {
           </div>
         </div>
       </div>
+
+      <Modal
+        isOpen={openDeleteAlert}
+        onClose={() => setOpenDeleteAlert(false)}
+        title="Delete Task"
+        >
+          <DeleteAlert
+            content="Are you sure you want to delete this task?"
+            onDelete={() => deleteTask()}
+          />
+        </Modal>
+
     </DashboardLayout>
   );
 };
